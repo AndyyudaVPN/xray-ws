b2469a6715bc5b49326143dd1f9a31e9dd1561b6
AndyyudaVPN <134195192+AndyyudaVPN@users.noreply.github.com>
2023-05-21 18:55:56 +0700

    Update trialssh.sh

--- trialssh.sh
+++ trialssh.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# SL
+
 # ==========================================
 # Color
 RED='\033[0;31m'
@@ -11,64 +11,113 @@ PURPLE='\033[0;35m'
 CYAN='\033[0;36m'
 LIGHT='\033[0;37m'
 # ==========================================
-# Getting
+export RED='\033[0;31m'
+export GREEN='\033[0;32m'
+export YELLOW='\033[0;33m'
+export BLUE='\033[0;34m'
+export PURPLE='\033[0;35m'
+export CYAN='\033[0;36m'
+export LIGHT='\033[0;37m'
+export NC='\033[0m'
+export ungu='\033[0;35m'
+
+# izin
 MYIP=$(wget -qO- ipinfo.io/ip);
-echo "Checking VPS"
-IZIN=$( curl ipinfo.io | grep $MYIP )
-if [ $MYIP = $MYIP ]; then
-echo -e "${NC}${GREEN}Permission Accepted...${NC}"
+echo "memeriksa vps anda"
+sleep 0.5
+CEKEXPIRED () {
+        today=$(date -d +1day +%Y -%m -%d)
+        Exp1=$(curl -sS https://raw.githubusercontent.com/AndyyudaVPN/xray-ws/main/izin | grep $MYIP | awk '{print $3}')
+        if [[ $today < $Exp1 ]]; then
+        echo "status script aktif.."
+        else
+        echo "SCRIPT ANDA EXPIRED";
+        exit 0
+fi
+}
+IZIN=$(curl -sS https://raw.githubusercontent.com/AndyyudaVPN/xray-ws/main/izin | awk '{print $4}' | grep $MYIP)
+if [ $MYIP = $IZIN ]; then
+echo "IZIN DI TERIMA!!"
+CEKEXPIRED
 else
-echo -e "${NC}${RED}Permission Denied!${NC}";
-echo -e "${NC}${LIGHT}Fuck You!!"
+echo "Akses di tolak!! Benget sia hurung!!";
 exit 0
 fi
-source /var/crot/ipvps.conf
-if [[ "$IP2" = "" ]]; then
+
+# Getting
+Login=trial`</dev/urandom tr -dc X-Z0-9 | head -c4`
+masaaktif="1"
+Pass="1"
+max="2"
 domain=$(cat /etc/xray/domain)
-else
-domain=$IP2
-fi
+sldomain=$(cat /root/nsdomain)
+cdndomain=$(cat /root/awscdndomain)
+slkey=$(cat /etc/slowdns/server.pub)
 clear
+
+echo "Script AutoCreate Akun SSH dan OpenVPN By CyberVPN"
+sleep 3
+echo Ping Host
+echo Cek Hak Akses...
+sleep 0.5
+echo Permission Accepted
+clear
+sleep 0.5
+echo Membuat Akun: $Login
+sleep 0.5
+echo Setting Password: $Pass
+sleep 0.5
 IP=$(wget -qO- ipinfo.io/ip);
+ws="$(cat ~/log-install.txt | grep -w "Websocket TLS" | cut -d: -f2|sed 's/ //g')"
+ws2="$(cat ~/log-install.txt | grep -w "Websocket None TLS" | cut -d: -f2|sed 's/ //g')"
+
 ssl="$(cat ~/log-install.txt | grep -w "Stunnel5" | cut -d: -f2)"
 sqd="$(cat ~/log-install.txt | grep -w "Squid" | cut -d: -f2)"
 ovpn="$(netstat -nlpt | grep -i openvpn | grep -i 0.0.0.0 | awk '{print $4}' | cut -d: -f2)"
 ovpn2="$(netstat -nlpu | grep -i openvpn | grep -i 0.0.0.0 | awk '{print $4}' | cut -d: -f2)"
-Login=Trial`</dev/urandom tr -dc X-Z0-9 | head -c4`
-hari="1"
-Pass=1
 clear
-systemctl restart ws-tls
-systemctl restart ws-nontls
+systemctl stop client-sldns
+systemctl stop server-sldns
+pkill sldns-server
+pkill sldns-client
+systemctl enable client-sldns
+systemctl enable server-sldns
+systemctl start client-sldns
+systemctl start server-sldns
+systemctl restart client-sldns
+systemctl restart server-sldns
+
 systemctl restart ssh-ohp
+systemctl restart rc-local
 systemctl restart dropbear-ohp
 systemctl restart openvpn-ohp
 useradd -e `date -d "$masaaktif days" +"%Y-%m-%d"` -s /bin/false -M $Login
-exp="$(chage -l $Login | grep "Account expires" | awk -F": " '{print $2}')"
+expi="$(chage -l $Login | grep "Account expires" | awk -F": " '{print $2}')"
+echo -e "$Pass\n$Pass\n"|passwd $Login &> /dev/null
 hariini=`date -d "0 days" +"%Y-%m-%d"`
 expi=`date -d "$masaaktif days" +"%Y-%m-%d"`
-echo -e "$Pass\n$Pass\n"|passwd $Login &> /dev/null
+clear
 echo -e ""
 echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
-echo -e "\E[44;1;39m                 ⇱ INFORMASI AKUN SSH ⇲            \E[0m"
+echo -e "\E[44;1;39m                 ⇱ TRIAL AKUN SSH ⇲            \E[0m"
 echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
 echo -e "${LIGHT}Username: $Login"
 echo -e "Password: $Pass"
 echo -e "Created: $hariini"
 echo -e "Expired: $expi"
-echo -e "${LIGHT}=================HOST-SSH===================="
+echo -e "${LIGHT}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
 echo -e "IP/Host: $IP"
 echo -e "Domain SSH: $domain"
 echo -e "Domain Cloudflare: $domain"
 echo -e "PubKey : $slkey"
 echo -e "Nameserver: $sldomain"
-echo -e "${LIGHT}===============Service-Port=================="
+echo -e "${LIGHT}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
 echo -e "OpenSSH: 22"
 echo -e "Dropbear: 44, 69, 143"
 echo -e "STunnel4: 442,222,2096"
-echo -e "SlowDNS port: 53,5300,8080"
+echo -e "SlowDNS port: 53,5300,8080,443,80"
 echo -e "SSH Websocket SSL/TLS: 443"
-echo -e "SSH Websocket HTTP: 80,8080"
+echo -e "SSH Websocket HTTP: 80,8080,8880"
 echo -e "SSH Websocket Direct: 8080"
 echo -e "OPEN VPN: 1194"
 echo -e "BadVPN UDPGW: 7100,7200,7300"
@@ -77,15 +126,16 @@ echo -e "Proxy Squid: [ON]"
 echo -e "OVPN TCP: http://$IP:81/tcp.ovpn"
 echo -e "OVPN UDP: http://$IP:81/udp.ovpn"
 echo -e "OVPN SSL: http://$IP:81/ssl.ovpn"
-echo -e "=================================================="
+echo -e "${LIGHT}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
 echo -e "SNI/Server Spoof: isi dengan bug"
 echo -e "Payload Websocket SSL/TLS"
-echo -e "=================================================="
+echo -e "${LIGHT}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
 echo -e "GET wss://bug.com/ HTTP/1.1[crlf]Host: [host][crlf]Upgrade: websocket[crlf][crlf]"
-echo -e "=================================================="
+echo -e "${LIGHT}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
 echo -e "Payload Websocket HTTP"
-echo -e "=================================================="
+echo -e "${LIGHT}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
 echo -e "GET / HTTP/1.1[crlf]Host: [host][crlf]Upgrade: websocket[crlf][crlf]"
-echo -e "=================================================="
-echo -e "${CYAN}       Script By Andy Yuda" 
-echo -e "${LIGHT}=================================================="
+echo -e "${LIGHT}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
+echo -e "${CYAN}     Terimakasih sudah menggunakan" 
+echo -e "${CYAN}        script premium Andyyuda"
+echo -e "${LIGHT}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━${NC}"
