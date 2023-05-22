7ae5a6e09a060f191f8d002501c3d85b76a1a08b
AndyyudaVPN <134195192+AndyyudaVPN@users.noreply.github.com>
2023-05-22 16:11:02 +0700

    Update cf.sh

--- cf.sh
+++ cf.sh
@@ -1,27 +1,30 @@
 #!/bin/bash
-# ==========================================
-# Color
-RED='\033[0;31m'
-NC='\033[0m'
-#GREEN='\033[0;32m'
-#ORANGE='\033[0;33m'
-BLUE='\033[0;34m'
-PURPLE='\033[0;35m'
-#CYAN='\033[0;36m'
-LIGHT='\033[0;37m'
-off='\x1b[m'
-# ==========================================
+# // String / Request Data
 # Getting
-
+MYIP=$(wget -qO- ipinfo.io/ip);
 clear
+apt install update && apt upgrade -y
 apt install jq curl -y
-DOMAIN=friendvps.my.id
-sub=$(</dev/urandom tr -dc a-z0-9 | head -c6)
-SUB_DOMAIN=${sub}.friendvps.my.id
-CF_ID=ralzikri21@gmail.com
-CF_KEY=ebdaf7136ace30fe9c57e957b93dab34f455e
+#sub=$(</dev/urandom tr -dc a-z | head -c4)
+clear
+echo -e ""
+echo -e "jangan karakter singkat seperti: sg, id, hk,"
+echo -e "kalau bisa 1 kata yang unik dengan dikombinasikan dengan angka"
+echo -e "contoh: resa11"
+echo -e ""
+echo -e "\e[32msubdomain\e[0m.andriwrt.me"
+read -p "Mau subdomain apa?( 1kata ) : " sub
+if [[ $sub == "" ]]; then
+clear
+echo -e "${EROR} No Input Detected !"
+exit 1
+fi
+DOMAIN=andriwrt.me
+SUB_DOMAIN=${sub}.andriwrt.me
+CF_ID=razertech52@gmail.com
+CF_KEY=8b0683c1ff3f6eed8dc32a70dfd2c02c80e9f
 set -euo pipefail
-IP=$(wget -qO- ipinfo.io/ip);
+IP=$(curl -sS ifconfig.me);
 echo "Updating DNS for ${SUB_DOMAIN}..."
 ZONE=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones?name=${DOMAIN}&status=active" \
      -H "X-Auth-Email: ${CF_ID}" \
@@ -46,38 +49,13 @@ RESULT=$(curl -sLX PUT "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_r
      -H "X-Auth-Key: ${CF_KEY}" \
      -H "Content-Type: application/json" \
      --data '{"type":"A","name":"'${SUB_DOMAIN}'","content":"'${IP}'","ttl":120,"proxied":false}')
-
-WILD_DOMAIN="*.$sub"
-set -euo pipefail
-echo ""
-echo "Updating DNS for ${WILD_DOMAIN}..."
-ZONE=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones?name=${DOMAIN}&status=active" \
-     -H "X-Auth-Email: ${CF_ID}" \
-     -H "X-Auth-Key: ${CF_KEY}" \
-     -H "Content-Type: application/json" | jq -r .result[0].id)
-
-RECORD=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records?name=${WILD_DOMAIN}" \
-     -H "X-Auth-Email: ${CF_ID}" \
-     -H "X-Auth-Key: ${CF_KEY}" \
-     -H "Content-Type: application/json" | jq -r .result[0].id)
-
-if [[ "${#RECORD}" -le 10 ]]; then
-     RECORD=$(curl -sLX POST "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records" \
-     -H "X-Auth-Email: ${CF_ID}" \
-     -H "X-Auth-Key: ${CF_KEY}" \
-     -H "Content-Type: application/json" \
-     --data '{"type":"A","name":"'${WILD_DOMAIN}'","content":"'${IP}'","ttl":120,"proxied":false}' | jq -r .result.id)
-fi
-
-RESULT=$(curl -sLX PUT "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records/${RECORD}" \
-     -H "X-Auth-Email: ${CF_ID}" \
-     -H "X-Auth-Key: ${CF_KEY}" \
-     -H "Content-Type: application/json" \
-     --data '{"type":"A","name":"'${WILD_DOMAIN}'","content":"'${IP}'","ttl":120,"proxied":false}')
+     
 echo "Host : $SUB_DOMAIN"
-echo $SUB_DOMAIN > /root/domain
-# / / Make Main Directory
-mkdir -p /usr/bin/xray
-mkdir -p /etc/xray
-cp /root/domain /etc/xray
-rm -f /root/cf.sh
+sleep 1
+yellow() { echo -e "\\033[33;1m${*}\\033[0m"; }
+yellow "Domain added.."
+sleep 3
+echo -e ""
+echo -e "subdomainmu telah jadi yaitu: $SUB_DOMAIN"
+echo -e "Pakai itu untuk install nanti"
+cd
