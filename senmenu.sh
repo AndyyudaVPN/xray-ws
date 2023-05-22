8a8cd5532b20a3fc320556e902000bcb8c1f2ee7
AndyyudaVPN <134195192+AndyyudaVPN@users.noreply.github.com>
2023-05-22 16:45:51 +0700

    Update senmenu.sh

--- senmenu.sh
+++ senmenu.sh
@@ -28,7 +28,7 @@ wget -q -O /usr/bin/menu-trojan "https://raw.githubusercontent.com/AndyyudaVPN/x
 wget -q -O /usr/bin/menu-ssh "https://raw.githubusercontent.com/AndyyudaVPN/xray-ws/main/menu-ssh.sh"
 #wget -q -O /usr/bin/menu-bckp "https://raw.githubusercontent.com/Agunxzzz/XrayCol/main/minacantik/menu-bckp-telegram.sh"
 wget -q -O /usr/bin/menu-backup "https://raw.githubusercontent.com/AndyyudaVPN/xray-ws/main/menu-backup.sh"
-#wget -q -O /usr/bin/bckp "https://raw.githubusercontent.com/Agunxzzz/XrayCol/main/minacantik/bckpbot.sh"
+wget -q -O /usr/bin/slow "https://raw.githubusercontent.com/AndyyudaVPN/xray-ws/main/slow.sh"
 wget -q -O /usr/bin/usernew "https://raw.githubusercontent.com/AndyyudaVPN/xray-ws/main/usernew.sh"
 wget -q -O /usr/bin/menu "https://raw.githubusercontent.com/AndyyudaVPN/xray-ws/main/menu.sh"
 wget -q -O /usr/bin/wbm "https://raw.githubusercontent.com/AndyyudaVPN/xray-ws/main/webmin.sh"
