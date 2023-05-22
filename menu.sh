5e628344d880f1f395dbf72bbc9870b8e4c7fe0c
AndyyudaVPN <134195192+AndyyudaVPN@users.noreply.github.com>
2023-05-22 16:40:43 +0700

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -220,7 +220,8 @@ echo -e " ${BICyan}[${BIWhite}08${BICyan}]${RED} •${NC} ${YELLOW}REBOOT       
 echo -e " ${BICyan}[${BIWhite}09${BICyan}]${RED} •${NC} ${YELLOW}RESTART ${GREEN}SERVICE $NC  ${BICyan}[${BIWhite}20${BICyan}]${RED} • ${NC}${YELLOW}SCRIPT INFO $NC"
 echo -e " ${BICyan}[${BIWhite}10${BICyan}]${RED} •${NC} ${YELLOW}TRIAL           $NC  ${BICyan}[${BIWhite}21${BICyan}]${RED} • ${NC}${YELLOW}CLEAR LOG $NC"
 echo -e " ${BICyan}[${BIWhite}11${BICyan}]${RED} •${NC} ${YELLOW}ADD ${GREEN}HOST        $NC  ${BICyan}[${BIWhite}22${BICyan}]${RED} • ${NC}${YELLOW}FIX MISSING POINTING $NC"
-echo -e " ${BICyan}[${BIWhite} X ${BICyan}] TYPE X FOR EXIT ${BICyan}${BIYellow}${BICyan}${NC}"  
+echo -e " ${BICyan}[${BIWhite} X ${BICyan}] TYPE X FOR EXIT ${BICyan}${BIYellow}${BICyan}${NC}"  $NC  ${BICyan}[${BIWhite}23${BICyan}]${RED} • ${NC}${YELLOW}GANTI NAMESAVER SLOWDNS $NC"
+
 echo -e " ${RED}"
 echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
 
@@ -259,6 +260,7 @@ case $opt in
 21) clear ; clearlog ;;
 #99) clear ; update ;;
 22) clear ; fix ;;
+23) clear ; slow ;;
 
 0) clear ; menu ;;
 x) exit ;;
