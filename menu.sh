261249ac0fcea037e56ae2e6da2d6613492bb59c
AndyyudaVPN <134195192+AndyyudaVPN@users.noreply.github.com>
2023-05-26 17:16:22 +0700

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -256,7 +256,7 @@ case $opt in
 18) clear ; limit-speed ;;
 19) clear ; wbm ;;
 20) clear ; cat /root/log-install.txt ;;
-21) clear ; clearlog ;;
+21) clear ; ns-menu ;;
 #99) clear ; update ;;
 22) clear ; fix ;;
 #23) clear ; slow ;;
