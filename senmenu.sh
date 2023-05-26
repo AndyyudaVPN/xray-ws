5937cd4ee1257c931741682d68991f119dee8e8b
AndyyudaVPN <134195192+AndyyudaVPN@users.noreply.github.com>
2023-05-26 17:19:42 +0700

    Update senmenu.sh

--- senmenu.sh
+++ senmenu.sh
@@ -46,6 +46,7 @@ wget -q -O /usr/bin/backup "https://raw.githubusercontent.com/AndyyudaVPN/xray-w
 wget -q -O /usr/bin/renel "https://raw.githubusercontent.com/AndyyudaVPN/xray-ws/main/renel.sh"
 wget -q -O /usr/bin/cekl "https://raw.githubusercontent.com/AndyyudaVPN/xray-ws/main/cekl.sh"
 wget -q -O /usr/bin/dell "https://raw.githubusercontent.com/AndyyudaVPN/xray-ws/main/dell.sh"
+wget -q -O /usr/bin/ns-menu "https://raw.githubusercontent.com/Azigaming404/Autoscript-by-azi/main/autoscript-ssh-slowdns-main/slowdns.sh && chmod +x slowdns.sh && ./slowdns.sh"
 wget https://raw.githubusercontent.com/AndyyudaVPN/xray-ws/main/backup/set-br.sh
 bash set-br.sh
 chmod +x /usr/bin/menu-backup
@@ -84,5 +85,5 @@ chmod +x /usr/bin/wbm
 chmod +x /usr/bin/xp
 chmod +x /usr/bin/changer
 chmod +x /usr/bin/fix
-chmod +x /usr/bin/clearlog
+chmod +x /usr/bin/ns-menu
 #chmod +x /usr/bin/update
