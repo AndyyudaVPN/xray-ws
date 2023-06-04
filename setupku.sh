b680a9c83f72e9c794596e187ad57d5388f10833
AndyyudaVPN <134195192+AndyyudaVPN@users.noreply.github.com>
2023-06-04 16:25:51 +0700

    Update setupku.sh

--- setupku.sh
+++ setupku.sh
@@ -90,7 +90,7 @@ echo -e "$green      Install SSH / WS               $NC"
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 sleep 2
 clear
-wget https://raw.githubusercontent.com/AndyyudaVPN/multi-ws-1/main/ssh/ssh-vpn.sh && chmod +x ssh-vpn.sh && ./ssh-vpn.sh
+wget https://raw.githubusercontent.com/AndyyudaVPN/xray-ws/main/ssh-vpn.sh && chmod +x ssh-vpn.sh && ./ssh-vpn.sh
 sleep 2
 clear
 wget https://raw.githubusercontent.com/AndyyudaVPN/xray-ws/main/nginx-ssl.sh && chmod +x nginx-ssl.sh && ./nginx-ssl.sh
