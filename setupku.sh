52ccc4ed02f11263b37376c9d1ee62b6c5ec455d
AndyyudaVPN <134195192+AndyyudaVPN@users.noreply.github.com>
2023-07-25 23:14:54 +0700

    Update setupku.sh

--- setupku.sh
+++ setupku.sh
@@ -43,6 +43,8 @@ sudo apt install vnstat -y
 wget -q https://raw.githubusercontent.com/AndyyudaVPN/xray-ws/main/tools.sh && chmod +x tools.sh && ./tools.sh
 rm tools.sh
 clear
+# domain random
+CDN="https://raw.githubusercontent.com/wingvpn/v4/main/ssh"
 # izin
 MYIP=$(wget -qO- ipinfo.io/ip);
 echo "memeriksa vps anda"
@@ -66,23 +68,38 @@ echo "Akses di tolak!! Benget sia hurung!!";
 exit 0
 fi
 
-clear
-echo "Add Domain for vmess/vless/trojan dll"
 echo " "
-read -rp "Input ur domain : " -e pp
-    if [ -z $pp ]; then
-        echo -e "
-        Nothing input for domain!
-        Then a random domain will be created"
-    else
-        echo "$pp" > /root/scdomain
-	echo "$pp" > /etc/xray/scdomain
-	echo "$pp" > /etc/xray/domain
-	echo "$pp" > /etc/v2ray/domain
-	echo $pp > /root/domain
-        echo "IP=$pp" > /var/lib/scrz-prem/ipvps.conf
+clear
+    echo -e "$BBlue                     SETUP DOMAIN VPS     $NC"
+    echo -e "$BYellow----------------------------------------------------------$NC"
+    echo -e "$BGreen 1. Use Domain Random / Gunakan Domain Random $NC"
+    echo -e "$BGreen 2. Choose Your Own Domain / Gunakan Domain Sendiri $NC"
+    echo -e "$BYellow----------------------------------------------------------$NC"
+    read -rp " input 1 or 2 / pilih 1 atau 2 : " dns
+	if test $dns -eq 1; then
+    clear
+    apt install jq curl -y
+    wget -q -O /root/cf "${CDN}/cf" >/dev/null 2>&1
+    chmod +x /root/cf
+    bash /root/cf | tee /root/install.log
+    print_success "Domain Random Done"
+	elif test $dns -eq 2; then
+    read -rp "Enter Your Domain / masukan domain : " dom
+    read -rp "Input ur ns-domain : " -e nsdomen
+    echo "IP=$dom" > /var/lib/scrz-prem/ipvps.conf
+    echo "$dom" > /root/scdomain
+	echo "$dom" > /etc/xray/scdomain
+	echo "$dom" > /etc/xray/domain
+	echo "$dom" > /etc/v2ray/domain
+	echo "$dom" > /root/domain
+        echo "$nsdomen" > /etc/xray/nsdomain
+        echo "$nsdomen" > /root/nsdomain
+    else 
+    echo "Not Found Argument"
+    exit 1
     fi
-
+	echo -e "${BGreen}Done!${NC}"
+    sleep 2
 clear
 #install ssh ovpn
 echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
